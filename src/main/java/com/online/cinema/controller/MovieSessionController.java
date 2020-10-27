8b1998d3bf46e41622cae48e3921c3bf35bc9711
Andriy <andriy.lagun@gmail.com>
2020-10-27 14:19:43 +0200

    travis fix

--- src/main/java/com/online/cinema/controller/MovieSessionController.java
+++ src/main/java/com/online/cinema/controller/MovieSessionController.java
@@ -8,7 +8,12 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/movie-sessions")
 @RestController
